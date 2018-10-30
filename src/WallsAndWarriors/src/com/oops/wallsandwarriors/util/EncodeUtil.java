356a92a42ac08aa1c44954d45ff9ae8f8bf24ccb
Cagla Sozen <cagla.sozen@ug.bilkent.edu.tr>
2018-10-30 10:10:00 +0300

    Update EncodeUtil.java

--- src/WallsAndWarriors/src/com/oops/wallsandwarriors/util/EncodeUtil.java
+++ src/WallsAndWarriors/src/com/oops/wallsandwarriors/util/EncodeUtil.java
@@ -12,25 +12,21 @@ public class EncodeUtil {
 
     private static String base64;
     private static ChallengeData data;
-    private static int challengeNo = 0;
 
     public static String encode(ChallengeData toEncode) throws FileNotFoundException, IOException
     {
-        challengeNo++;
-        String tag = "chl"+challengeNo+".txt";
+        String tag = "chl"+toEncode.name +".txt";
         PrintWriter out = new PrintWriter(tag);
 
         data = toEncode;
-        byte[] inBytes = getbyteStream(data);
+        byte[] inBytes = getByteStream(data);
 
         base64 = Base64.getEncoder().encodeToString(inBytes);
         out.println(base64);
         return base64;
     }
 
-
-
-    private static byte[] getbyteStream(ChallengeData toByte) throws IOException
+    private static byte[] getByteStream(ChallengeData toByte) throws IOException
     {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ObjectOutput out = null;
@@ -45,9 +41,48 @@ public class EncodeUtil {
                 bos.close();
             } catch (IOException ex) {
                 {
-                    System.out.println("BYTE STREAMING EXCEPTION");
+                    System.out.println("TO BYTE STREAMING EXCEPTION");
                 }
             }
         }
     }
+
+    public static ChallengeData decode(String toDecode) throws FileNotFoundException, IOException, ClassNotFoundException
+    {
+        String tag = toDecode;
+        BufferedReader in = new BufferedReader(new FileReader(tag));
+        String line;
+        String str = "";
+        while((line = in.readLine()) != null)
+        {
+           str += line;
+        }
+        in.close();
+
+        byte[] byteArr = str.getBytes();
+
+        data = fromByteStream(byteArr);
+        return data;
+
+    }
+
+    private static ChallengeData fromByteStream(byte[] toObj) throws IOException,ClassNotFoundException
+    {
+        ByteArrayInputStream bis = new ByteArrayInputStream(toObj);
+        ObjectInput in = null;
+        try {
+            in = new ObjectInputStream(bis);
+            Object o = in.readObject();
+            return (ChallengeData) o;
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                System.out.println("FROM BYTE STREAMING EXCEPTION");
+            }
+        }
+    }
+
 }
