14e1e876a41a5ebcf719e02cce580b6ccb80c6b4
MerveSagyatanlar <merv.sagyatanlar@gmail.com>
2018-11-17 17:22:27 +0300

    Added screens

--- src/WallsAndWarriors/src/com/oops/wallsandwarriors/screens/CreditsScreen.java
+++ src/WallsAndWarriors/src/com/oops/wallsandwarriors/screens/CreditsScreen.java
@@ -2,8 +2,11 @@ package com.oops.wallsandwarriors.screens;
 
 import com.oops.wallsandwarriors.util.DebugUtils;
 import com.oops.wallsandwarriors.Game;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 
 public class CreditsScreen extends GeneralScreen {
     
@@ -14,9 +17,29 @@ public class CreditsScreen extends GeneralScreen {
         
         DebugUtils.initClickDebugger(scene);
         addBackgroundCanvas(root, "resources/images/background2.png", "Credits");
-        addTransactionButton(root, "Back", 300, 300, Game.getInstance().screenManager.mainMenu);
-        
+        addTransactionButton(root, "Back", 700, 550, Game.getInstance().screenManager.mainMenu);
+        addNameList(root);
+
         return scene;
+    }
+
+    public void addNameList(Group root){
+        ListView<String> nameList = new ListView<String>();
+        ObservableList<String> names = FXCollections.observableArrayList(
+                "Ali Babayev                 ali.babayev@ug.bilkent.edu.tr",
+                    "Tunar Mahmudov       tunar.mahmudov@ug.bilkent.edu.tr",
+                    "Merve Sagyatanlar     merve.sagyatanlar@ug.bilkent.edu.tr",
+                    "Cagla Sozen               cagla.sozen@ug.bilkent.edu.tr",
+                    "Bahadir Tuluce           bahadir.tuluce@ug.bilkent.edu.tr"
+                 );
+        nameList.setItems(names);
+        nameList.setLayoutX(100);
+        nameList.setLayoutY(250);
+
+        nameList.setPrefSize(600,130);
+
+        root.getChildren().add(nameList);
+
     }
     
 }
