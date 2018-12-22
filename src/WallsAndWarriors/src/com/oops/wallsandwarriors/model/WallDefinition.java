12194cdd9fbc8699e260b76797f363d4acf893bb
Cagla Sozen <cagla.sozen@ug.bilkent.edu.tr>
2018-12-22 12:08:13 +0300

    Added JavaDoc comments

--- src/WallsAndWarriors/src/com/oops/wallsandwarriors/model/WallDefinition.java
+++ src/WallsAndWarriors/src/com/oops/wallsandwarriors/model/WallDefinition.java
@@ -1,3 +1,11 @@
+/**
+ * A class to be used to define the wall shapes. This class stores Lists of two
+ * objects, one of WallBastion type and the other of WallPortion type to define
+ * the wall shape.
+ * Implements Serializable
+ * @author OOPs
+ * @version 21.12.19
+ */
 package com.oops.wallsandwarriors.model;
 
 import java.io.Serializable;
@@ -7,12 +15,23 @@ public class WallDefinition implements Serializable{
 
     public final List<WallPortion> portions;
     public final List<WallBastion> bastions;
-    
+
+    /**
+     * A constructor that initializes a WallDefinition with the given portions and bastions.
+     * @param portions  The list of WallPortions for portions on the Wall.
+     * @param bastions The list of WallBastions for bastions on the Wall.
+     */
     public WallDefinition(List<WallPortion> portions, List<WallBastion> bastions) {
         this.portions = portions;
         this.bastions = bastions;
     }
 
+    /**
+     * An overriden equals method to check the equality of the WallDefinition objects.
+     * Equality for WallDefinition objects require equality of both portions and bastions.
+     * @param other Object to be compared, expected to be a WallDefinition.
+     * @return A boolean value to indicate whether the objects are equal or not.
+     */
     @Override
     public boolean equals(Object other) {
         if (other instanceof WallDefinition) {
