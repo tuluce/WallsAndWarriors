2fd092291b48ab81e5d0c08ead7b58fd122fb53a
Ali Babayev <ali.babayev@ug.bilkent.edu.tr>
2018-12-22 20:25:27 +0300

    Added javadoc comments

--- src/WallsAndWarriors/src/com/oops/wallsandwarriors/screens/Screen.java
+++ src/WallsAndWarriors/src/com/oops/wallsandwarriors/screens/Screen.java
@@ -2,8 +2,14 @@ package com.oops.wallsandwarriors.screens;
 
 import javafx.scene.Scene;
 
+/**
+ * An interface to define a generic function to get current screen
+ */
 public interface Screen {
 
+    /**
+     * A method to return the current Screen.
+     * @return the current screen as a Screen object.
+     */
     Scene getScene();
-    
 }
