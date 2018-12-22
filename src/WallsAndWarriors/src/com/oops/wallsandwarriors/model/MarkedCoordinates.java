3d86473e052708832dd82060b0d8bda0a0eb3ec4
Cagla Sozen <cagla.sozen@ug.bilkent.edu.tr>
2018-12-22 11:46:16 +0300

    Added JavaDoc comments

--- src/WallsAndWarriors/src/com/oops/wallsandwarriors/model/MarkedCoordinates.java
+++ src/WallsAndWarriors/src/com/oops/wallsandwarriors/model/MarkedCoordinates.java
@@ -1,3 +1,8 @@
+/**
+ * A class to store the marked Coordinates on the grid.
+ * @author OOPs
+ * @version 21.12.19
+ */
 package com.oops.wallsandwarriors.model;
 
 import java.util.ArrayList;
@@ -5,23 +10,41 @@ import java.util.ArrayList;
 public class MarkedCoordinates {
     public final ArrayList<Coordinate> markedCoordinates;
 
+    /**
+     * A default constructor that initializes a MarkedCoordinates with no given parameters
+     * with an ArrayList of Coordinate objects.
+     */
     public MarkedCoordinates() {
         markedCoordinates = new ArrayList<Coordinate>();
     }
 
+    /**
+     * Method to add a Coordinate as a MarkedCoordinate to the ArrayList of
+     * MarkedCoordinates.
+     * @param  newCoordinate Coordinate to be marked.
+     */
     public void addMarkedCoordinate(Coordinate newCoordinate) {
         markedCoordinates.add(newCoordinate);
     }
 
+    /**
+     * Method to find a Coordinate in the MarkedCoordinates
+     * to determine whether it is marked or not.
+     * @param  coordinate Coordinate to be searched.
+     * @return Result of the search as a boolean value.
+     */
     public boolean findCoordinate(Coordinate coordinate) {
-        for(int i = 0; i < markedCoordinates.size(); i++){
-            if(coordinate.equals(markedCoordinates.get(i))){
+        for (int i = 0; i < markedCoordinates.size(); i++) {
+            if (coordinate.equals(markedCoordinates.get(i))) {
                 return true;
             }
         }
         return false;
     }
 
+    /**
+     * Method to clear  marks on all coordinates.
+     */
     public void clearMarks() {
         markedCoordinates.clear();
     }
