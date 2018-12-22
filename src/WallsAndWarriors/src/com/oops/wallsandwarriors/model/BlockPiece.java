5eccc8c21b823833e42c63c9e78b2e357021212a
Cagla Sozen <cagla.sozen@ug.bilkent.edu.tr>
2018-12-22 10:31:57 +0300

    Added JavaDoc comments

--- src/WallsAndWarriors/src/com/oops/wallsandwarriors/model/BlockPiece.java
+++ src/WallsAndWarriors/src/com/oops/wallsandwarriors/model/BlockPiece.java
@@ -1,3 +1,9 @@
+/**An interface to define a generic structure for objects occupying some blocks of the grid.
+ * Extends GridPiece
+ * @author OOPs
+ * @version 21.12.19
+ */
+
 package com.oops.wallsandwarriors.model;
 
 import java.util.List;
