6cdeafae17cb046ffc432f37f4892bc705b3df28
Cagla Sozen <cagla.sozen@ug.bilkent.edu.tr>
2018-12-22 12:04:16 +0300

    Added JavaDoc comments

--- src/WallsAndWarriors/src/com/oops/wallsandwarriors/model/WallData.java
+++ src/WallsAndWarriors/src/com/oops/wallsandwarriors/model/WallData.java
@@ -1,3 +1,12 @@
+/**
+ * A class to store the structure and the position of a wall as a game object. This
+ * object has two attributes, definition in type WallDefinition and position in
+ * type Coordinate. These two attributes define the shape of a particular wall as
+ * described in the WallDefinition and its positioning on the grid.
+ * Implements BorderPiece, Serializable
+ * @author OOPs
+ * @version 21.12.19
+ */
 package com.oops.wallsandwarriors.model;
 
 import com.oops.wallsandwarriors.util.Point;
@@ -6,41 +15,76 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WallData implements BorderPiece, Serializable {
-    
+
+    private static final int ROTATION = 4;
+
     private WallDefinition definition;
     private Coordinate position;
-    
+
+    /**
+     * A constructor that initializes WallData with the given definition and position.
+     * @param definition  Definition of the wall as a WallDefinition.
+     * @param position Position of the wall as a Coordinate.
+     */
     public WallData(WallDefinition definition, Coordinate position) {
         this.definition = definition;
         this.position = position;
     }
-    
+
+    /**
+     * A constructor that initializes WallData with the given definition and with a null position.
+     * @param definition  Definition of the wall as a WallDefinition.
+     */
     public WallData(WallDefinition definition) {
         this(definition, null);
     }
-    
+
+    /**
+     * A get method to return the definition of the WallData.
+     * @return definition of the WallData
+     */
     public WallDefinition getWallDefinition() {
         return definition;
     }
-    
+
+    /**
+     * A get method to return the position of the WallData.
+     * @return position of the WallData
+     */
     public Coordinate getPosition() {
         return position;
     }
-    
+
+    /**
+     * An overriden method to set the position of the Wall on the grid as a GridPiece.
+     * @param position Coordinate on the grid to set the position of the Wall.
+     */
     @Override
     public void setPosition(Coordinate position) {
         this.position = position;
     }
 
+    /**
+     * A set method to modify the definition of the WallData.
+     * @param definition new definition of the WallData
+     */
     public void setWallDefinition(WallDefinition definition){
         this.definition = definition;
     }
-    
+
+    /**
+     * An overriden method to create a copy of the WallData as a GridPiece.
+     * @return the copy of the WallData.
+     */
     @Override
     public WallData createCopy() {
         return new WallData(definition, position);
     }
-    
+
+    /**
+     * An overriden method to rotate a Wall as a GridPiece. As a result of the rotation,
+     * wall definition is modified.
+     */
     public void rotate() {
         List<WallPortion> rotatedPortions = new ArrayList<WallPortion>();
         List<WallBastion> rotatedBastions = new ArrayList<WallBastion>();
@@ -57,12 +101,23 @@ public class WallData implements BorderPiece, Serializable {
         WallDefinition rotatedDefinition = new WallDefinition(rotatedPortions, rotatedBastions);
         this.definition = rotatedDefinition;
     }
-    
+
+    /**
+     * An overriden method to get the list of Points that a occupies a border
+     * by the Wall as a BorderPiece.
+     * @return  list of Points that a occupies a border by the Wall.
+     */
     @Override
     public List<Point> occupiesBorder() {
         return occupies(position);
     }
-    
+
+    /**
+     * A method to get the list of Points that a is occupied
+     * by a Wall according to the position of the Wall.
+     * @param position position of the Wall.
+     * @return  list of Points that is occupied by the Wall.
+     */
     public List<Point> occupies(Coordinate position) {
         List<Point> occupies = new ArrayList<Point>();
         if (position == null) {
@@ -88,11 +143,16 @@ public class WallData implements BorderPiece, Serializable {
         return occupies;
     }
 
+    /**
+     * A method to check if the WallData is equal to the given WallData
+     * regardless of the changes in the definition as a result of the rotations.
+     * @param otherWall WallData to be compared.
+     * @return  A boolean value that indicates whether the WallDatas are equal or not.
+     */
     public boolean isPieceEqual( WallData otherWall )
     {
         WallData copyWall = otherWall.createCopy();
-        for( int i = 0; i < 4; i++)
-        {
+        for ( int i = 0; i < ROTATION; i++) {
             if( this.getWallDefinition().equals(copyWall.getWallDefinition()) )
                 return true;
             copyWall.rotate();
