e8d95b33ddeb6175f19f521ffb577230677c6029
Cagla Sozen <cagla.sozen@ug.bilkent.edu.tr>
2018-12-22 10:33:17 +0300

    Added JavaDoc comments

--- src/WallsAndWarriors/src/com/oops/wallsandwarriors/model/BorderPiece.java
+++ src/WallsAndWarriors/src/com/oops/wallsandwarriors/model/BorderPiece.java
@@ -1,3 +1,10 @@
+/**
+ * An interface to define a generic structure for objects occupying some borders of
+ * the grid. 
+ * Extends GridPiece
+ * @author OOPs
+ * @version 21.12.19
+ */
 package com.oops.wallsandwarriors.model;
 
 import com.oops.wallsandwarriors.util.Point;
